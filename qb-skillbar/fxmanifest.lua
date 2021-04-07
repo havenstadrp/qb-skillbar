de95ad225f8edafa5e3a7d793ba0723ded630c59
GhzGarage <57848836+GhzGarage@users.noreply.github.com>
2021-04-07 17:23:11 -0500

    Initial Commit

---
+++ qb-skillbar/fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Skillbar'
+version '1.0.0'
+
+
+ui_page "html/index.html"
+
+client_scripts {
+    'client/main.lua',
+    'config.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+    'config.lua',
+}
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/VerdanaBold.ttf',
+}
+
+exports {
+    'GetSkillbarObject'
+}
+
+dependencies {
+    'qb-core'
+}
